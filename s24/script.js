0d997e33df484ecdb06b9d9b8c2d23b1b1502632
Niurka Obregon <niuobregonsalhuana@crackthecode.la>
2025-09-04 09:14:35 -0500

    Sesion 24 Practica de declaracion de variables

---
+++ s24/script.js
@@ -0,0 +1,31 @@
+console.log("Hola mundo");
+
+let desconocido;
+console.log(desconocido);
+console.log(typeof(desconocido));
+
+let verdadero=(true);
+let falso=false;
+console.log(verdadero);
+console.log(typeof(verdadero));
+
+let edad= 17;
+let estatura= 1.50;
+
+console.log(edad);
+console.log(typeof(edad));
+console.log(estatura);
+console.log(typeof(estatura));
+
+let nombre='Niurka Obregon';
+let frase="Ten un buen dia.";
+
+console.log(nombre);
+console.log(typeof(nombre));
+
+let reto1="Miguel Grau \"El caballero de los Mares\"";
+let reto2= 'It\'s me';
+
+console.log(reto1);
+console.log(reto2);
+
