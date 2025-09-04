dece50c12c241fcbd260adec80246ea7af449492
Niurka Obregon <niuobregonsalhuana@crackthecode.la>
2025-09-04 09:37:10 -0500

    Sesion 33 Practica de seleccion de elementos del DOM

---
+++ s33/assets/js/script.js
@@ -0,0 +1,7 @@
+let mainTitle=  document.getElementById("main-title");
+console.log(mainTitle);
+
+console.log(mainTitle.innerText);
+
+mainTitle.innerText="TITULO CAMBIADO";
+console.log(mainTitle.innerText);
