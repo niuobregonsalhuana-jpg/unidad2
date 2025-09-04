8af8b60db3e50c578451bb84371e4ee4f4ca16db
Niurka Obregon <niuobregonsalhuana@crackthecode.la>
2025-09-04 09:23:09 -0500

    Sesion 29 Practica de Arrays y bucles

---
+++ s29/assets/js/script.js
@@ -0,0 +1,13 @@
+console.log("Hola");
+
+let fruits = ["manzana", "platano", "mandarina", "mango", "uvas", "pera"];
+
+let numbers = [-5, -6, -7, -8, -9, -10, -11, -12, -13];
+
+
+
+let iterator = 167;
+while (iterator <= 349) {
+    console.log(iterator);
+    iterator += 24;
+} 
